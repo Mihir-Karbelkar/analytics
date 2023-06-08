f707d7dc4ac0d558abb1227ce32a9c741004cac8
mihir karbelkar <karbelkar.mihir@gmail.com>
2023-06-08 12:01:14 +0530

    fix api.ts

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,9 +1,6 @@
 export const checkEnvironment = () => {
-  let base_url = !!!process.env.API_BASE_URL
-    ? "http://localhost:3000"
-    : process.env.API_BASE_URL;
-  console.log(process.env.API_BASE_URL);
-  return base_url || "http://localhost:3000";
+  let base_url = process.env.API_BASE_URL;
+  return base_url || "";
 };
 export const api = (input: string, init?: RequestInit | undefined) => {
   return fetch(checkEnvironment()?.concat(input), init);
