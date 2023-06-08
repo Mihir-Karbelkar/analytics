a47b5ca368b7f7733ce915d1a8e1c9c10119781c
mihir karbelkar <karbelkar.mihir@gmail.com>
2023-06-08 11:30:13 +0530

    changes to api.ts

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,9 +1,8 @@
 export const checkEnvironment = () => {
-  let base_url =
-    process.env.NODE_ENV === "development"
-      ? "http://localhost:3000"
-      : process.env.API_BASE_URL; // https://v2ds.netlify.app
-
+  let base_url = !!!process.env.API_BASE_URL
+    ? "http://localhost:3000"
+    : process.env.API_BASE_URL;
+  console.log(process.env.API_BASE_URL);
   return base_url || "http://localhost:3000";
 };
 export const api = (input: string, init?: RequestInit | undefined) => {
